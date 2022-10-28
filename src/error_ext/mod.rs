5ae979f24b7ba21ecbe1a4e78d8e0fac7532f7eb
BurntNail <BurntNail@proton.me>
2022-10-28 23:25:04 +0100

    fix mod.rs

--- src/error_ext/mod.rs
+++ src/error_ext/mod.rs
@@ -1,12 +1,15 @@
+///All of the stuff from anyhow that I use
 mod anyhow_stuff;
 pub use anyhow_stuff::*;
 
 #[cfg(feature = "tracing")]
+///All of the impls with tracing
 mod tracing_stuff;
 #[cfg(feature = "tracing")]
 pub use tracing_stuff::*;
 
 #[cfg(not(feature = "tracing"))]
+///All of the impls without tracing
 mod no_tracing;
 #[cfg(not(feature = "tracing"))]
 pub use no_tracing::*;
